f3f7107f50d6ad75675c0cac41e9719d58163e49
superwang <superwangcarl@aliyun.com>
2019-05-27 16:51:12 +0800

    定时任务

---
+++ scheduled/src/main/java/com/carlme/scheduled/origin/ReportSynDataTask.java
@@ -0,0 +1,15 @@
+package com.carlme.scheduled.origin;
+
+import org.springframework.stereotype.Component;
+
+import java.util.TimerTask;
+
+@Component
+public class ReportSynDataTask extends TimerTask {
+
+	public void run() {
+		//定时任务执行..
+		System.out.println("run...");
+	}
+
+}
